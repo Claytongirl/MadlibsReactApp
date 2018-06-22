09468ff3c734f6b9a41df72a44f0a03323085cc7
Claytongirl <lisadeclayton@gmail.com>
2018-06-22 11:24:15 -0600

    Built our first class component for the inputs containter and built an inout funtional container to render custom components

---
+++ src/components/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Header from './header';
+import Card from './card';
+//don't have to put header.js, it knows
+
+class Home extends Component {
+  render() {
+    return (
+      <div className = "home">
+      {Header()}
+      <Card />
+      </div>
+    );
+  }
+}
+
+export default Home;
