dbc327741d83db905d8433657a8df1ce8424267b
Claytongirl <lisadeclayton@gmail.com>
2018-06-25 11:20:00 -0600

    Created content component to generate story

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Input from './input';
+import Content from './content';
 
 class Card extends Component {
 
@@ -29,7 +30,6 @@ class Card extends Component {
 
     handleInputChange(event) {
         this.setState({ [event.target.name]: event.target.value })
-        console.log(this.state);
     }
 
     render() {
@@ -60,6 +60,7 @@ class Card extends Component {
                 {
                     inputData.map(data => Input( (data), this.handleInputChange ))
                 }
+                <Content/>
             </div>
         )
         }
